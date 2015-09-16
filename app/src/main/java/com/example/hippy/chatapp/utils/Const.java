5f4d00b028dfcba9ac13cf8749ea1caa867d4a07
woekun <woekun@gmail.com>
2015-09-16 23:04:28 +0700

    userList: listView--->expandableListview +

--- app/src/main/java/com/example/hippy/chatapp/utils/Const.java
+++ app/src/main/java/com/example/hippy/chatapp/utils/Const.java
@@ -3,6 +3,8 @@ package com.example.hippy.chatapp.utils;
 public class Const {
     public static final String EXTRA_DATA = "extraData";
     public static final int REQUEST_CODE = 10;
-    public static final String APP_ID = "NfMATnvTTOfToqHzhbCdcnuNC6lWR8rdeZnubr6r";
-    public static final String CLIENT_KEY = "yZE3rDFbP1knKbICX72OnfDf8Qs6eJ9WnIzotUV2";
+//    public static final String APP_ID = "NfMATnvTTOfToqHzhbCdcnuNC6lWR8rdeZnubr6r";
+//    public static final String CLIENT_KEY = "yZE3rDFbP1knKbICX72OnfDf8Qs6eJ9WnIzotUV2";
+    public static final String APP_ID = "79sEAU8hFXbMAVU3LOVv71g2UtdF6d44t937SC12";
+    public static final String CLIENT_KEY = "8qIzVeg5CFKzA8mfOeaxOZrnAUiv5w0SuMHMhFtY";
 }
