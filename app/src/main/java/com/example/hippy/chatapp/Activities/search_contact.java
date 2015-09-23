3c1b8463648809ca178c7eeb06ed4cf7acca4783
woekun <woekun@gmail.com>
2015-09-24 05:31:55 +0700

    autocompletetextview -> multiautocompletetextview

--- app/src/main/java/com/example/hippy/chatapp/Activities/search_contact.java
+++ app/src/main/java/com/example/hippy/chatapp/Activities/search_contact.java
@@ -1,20 +1,12 @@
 package com.example.hippy.chatapp.Activities;
 
 import android.os.Bundle;
-
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.MultiAutoCompleteTextView;
 
 import com.example.hippy.chatapp.R;
 import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
@@ -22,7 +14,7 @@ import java.util.List;
 
 public class search_contact extends NavigationDrawer {
 
-    AutoCompleteTextView actSearch;
+    MultiAutoCompleteTextView mactSearch;
     ArrayList<String> contactsList;
     ArrayAdapter<String> arrayAdapter;
 
@@ -33,8 +25,8 @@ public class search_contact extends NavigationDrawer {
 
         contactsList = new ArrayList<>();
         arrayAdapter = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item);
-        actSearch = (AutoCompleteTextView) findViewById(R.id.actSearch);
-        actSearch.setThreshold(1);
+        mactSearch = (MultiAutoCompleteTextView) findViewById(R.id.mactSearch);
+
     }
 
     @Override
@@ -50,8 +42,8 @@ public class search_contact extends NavigationDrawer {
                                 contactsList.add(pu.getUsername());
 
                             arrayAdapter.addAll(contactsList);
-                            actSearch.setAdapter(arrayAdapter);
-
+                            mactSearch.setAdapter(arrayAdapter);
+                            mactSearch.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
                         }
                     }
                 });
