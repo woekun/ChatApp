8238edeac664c0c3e149f643f8165147bb4690ac
woekun <woekun@gmail.com>
2015-10-11 23:01:33 +0700

    change something

---
+++ app/src/main/java/com/example/hippy/chatapp/custom/BaseConnection.java
@@ -0,0 +1,51 @@
+package com.example.hippy.chatapp.custom;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+
+import com.example.hippy.chatapp.Activities.NavigationDrawer;
+import com.example.hippy.chatapp.Service.SinchService;
+
+
+public class BaseConnection extends NavigationDrawer implements ServiceConnection {
+
+    private SinchService.ServiceInterface serviceInterface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getApplicationContext().bindService(new Intent(this, SinchService.class),
+                this,BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+        if (SinchService.class.getName().equals(componentName.getClassName())) {
+            serviceInterface = (SinchService.ServiceInterface) iBinder;
+            onServiceConnected();
+        }
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName componentName) {
+        if (SinchService.class.getName().equals(componentName.getClassName())) {
+            serviceInterface = null;
+            onServiceDisconnected();
+        }
+    }
+
+    protected void onServiceConnected() {
+        // for subclasses
+    }
+
+    protected void onServiceDisconnected() {
+        // for subclasses
+    }
+
+    protected SinchService.ServiceInterface getSinchServiceInterface() {
+        return serviceInterface;
+    }
+}
